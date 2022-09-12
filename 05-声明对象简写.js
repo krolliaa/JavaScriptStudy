ef64cef97098db82227594ae9d9feafcd353d3fe
krolliaa <krolliaa@163.com>
2022-09-12 17:04:41 +0800

    声明对象简写

--- 05-声明对象简写.js
+++ 05-声明对象简写.js
@@ -1,14 +1,14 @@
-let age = 18
-let name = 'kk'
+let age = 18;
+let name = 'kk';
 // 传统
 let person1 = {
     age: age, 
     name: name
-}
+};
 console.log(person1)
 // ES6
 let person2 = {
     age, 
     name
-}
-console.log(person2)
+};
+console.log(person2);
